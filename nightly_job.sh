a636849033e831257cbba5a7b267af22d6d981b9
patrickvossler18 <patrick.vossler18@gmail.com>
2019-08-13 11:31:51 -0700

    fix paths for nightly bash script

--- nightly_job.sh
+++ nightly_job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo python3 fetch_papers.py
-sudo python3 download_pdfs.py
-sudo python3 parse_pdf_to_text.py
-sudo python3 thumb_pdf.py
-sudo python3 analyze.py
-sudo python3 buildsvm.py
-sudo python3 make_cache.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/fetch_papers.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/download_pdfs.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/parse_pdf_to_text.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/thumb_pdf.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/analyze.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/buildsvm.py
+sudo python3 /home/ubuntu/arxiv-sanity-preserver/make_cache.py
+
